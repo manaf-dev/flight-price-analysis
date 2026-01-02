6a78437a0f9b24365cf62117d1cb650f2e5fe76f
Manaf <manaf18.m@gmail.com>
2026-01-02 14:54:32 +0000

    load data with spark

---
+++ spark/spark_etl.py
@@ -0,0 +1,38 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import (
+    DoubleType,
+    IntegerType,
+    StringType,
+    StructField,
+    StructType,
+    TimestampType,
+)
+
+SCHEMA = StructType([
+    StructField('Airline', StringType(), True), 
+    StructField('Source', StringType(), True), 
+    StructField('Source Name', StringType(), True), 
+    StructField('Destination', StringType(), True), 
+    StructField('Destination Name', StringType(), True), 
+    StructField('Departure Date & Time', TimestampType(), True), 
+    StructField('Arrival Date & Time', TimestampType(), True), 
+    StructField('Duration (hrs)', DoubleType(), True), 
+    StructField('Stopovers', StringType(), True), 
+    StructField('Aircraft Type', StringType(), True), 
+    StructField('Class', StringType(), True), 
+    StructField('Booking Source', StringType(), True), 
+    StructField('Base Fare (BDT)', DoubleType(), True), 
+    StructField('Tax & Surcharge (BDT)', DoubleType(), True), 
+    StructField('Total Fare (BDT)', DoubleType(), True), 
+    StructField('Seasonality', StringType(), True), 
+    StructField('Days Before Departure', IntegerType(), True)
+])
+
+
+def extract_data(spark):
+    return spark.read.csv('./data/Flight_Price_Dataset_of_Bangladesh.csv', header=True, schema=SCHEMA)
+    
+
+def initialize_spark():
+    spark = SparkSession.builder.appName('flight-price-analysis').getOrCreate()
+    return spark
